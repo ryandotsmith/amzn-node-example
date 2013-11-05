b6dd9798bc08849352273c8846fa0a741550c0c2
Ryan Smith <r@32k.io>
2013-11-04 18:17:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 http = require('http');
+util = require('util');
 
 server = http.createServer(function(req, resp) {
+	time = new Date();
 	resp.setHeader("Content-Type", "text/plain");
-	resp.end("Hello World\n");
+	resp.end(util.format("Hello World. The time is now: %s", time));
 });
 
 server.listen("/home/deploy/var/socket/nginx.socket", 128);
